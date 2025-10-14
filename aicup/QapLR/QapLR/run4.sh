52177f255cb365ceb0d4f4769607d9229800f86b
adler3d <adler3d@gmail.com>
2025-10-14 17:37:02 +0300

    upd

--- aicup/QapLR/QapLR/run4.sh
+++ aicup/QapLR/QapLR/run4.sh
@@ -1,6 +1,6 @@
-./QapLR.exe t_splinter 4 9 0 -p 31000 -o replay.cmds &
-echo ./socket_adapter.exe -e 127.0.0.1:31000 ./adler20250907.exe&
-./socket_adapter.exe -e 127.0.0.1:31000 ./gpt5.exe&
-./socket_adapter.exe -e 127.0.0.1:31001 ./gpt5.exe&
-./socket_adapter.exe -e 127.0.0.1:31002 ./gpt5.exe&
-./socket_adapter.exe -e 127.0.0.1:31003 ./gpt5.exe&
+./QapLR.exe t_splinter 4 9 0 -p 31500 -o replay.cmds &
+echo ./socket_adapter.exe -e 127.0.0.1:31500 ./adler20250907.exe&
+./socket_adapter.exe -e 127.0.0.1:31500 ./gpt5.exe&
+./socket_adapter.exe -e 127.0.0.1:31501 ./gpt5.exe&
+./socket_adapter.exe -e 127.0.0.1:31502 ./gpt5.exe&
+./socket_adapter.exe -e 127.0.0.1:31503 ./gpt5.exe&
