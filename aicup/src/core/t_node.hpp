781ca33da9010496768efadf667cd695557521ba
adler3d <adler3d@gmail.com>
2025-10-19 20:01:01 +0300

    upd

--- aicup/src/core/t_node.hpp
+++ aicup/src/core/t_node.hpp
@@ -198,6 +198,8 @@ struct t_node:t_process,t_node_cache{
       void stop() {
           if (stdin_file) { fclose(stdin_file); stdin_file = nullptr; }
           if (stdout_file) { fclose(stdout_file); stdout_file = nullptr; }
+          qap_close(stdin_fd);
+          qap_close(stdout_fd);
           if (pid > 0) {
               kill_by_pid(pid);
               pid = -1;
