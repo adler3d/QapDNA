1806309f357b63bbfe0242a5060c22e3a7caa07c
adler3d <adler3d@gmail.com>
2025-10-21 09:33:42 +0300

    upd

--- aicup/src/core/core.cpp
+++ aicup/src/core/core.cpp
@@ -216,6 +216,72 @@ struct t_game_decl{
   #include "defprovar.inl"
   //===
 };
+struct t_game_uploaded_ack{
+  #define DEF_PRO_COPYABLE()
+  #define DEF_PRO_CLASSNAME()t_game_uploaded_ack
+  #define DEF_PRO_VARIABLE(ADD)\
+  ADD(int,game_id,{})\
+  ADD(string,err,{})\
+  //===
+  #include "defprovar.inl"
+  //===
+};
+struct t_finished_game{
+  #define DEF_PRO_COPYABLE()
+  #define DEF_PRO_CLASSNAME()t_finished_game
+  #define DEF_PRO_VARIABLE(ADD)\
+  ADD(int,game_id,{})\
+  ADD(vector<double>,slot2ms,{})\
+  ADD(vector<double>,slot2score,{})\
+  ADD(vector<string>,slot2status,{})\
+  ADD(int,tick,0)\
+  ADD(int,size,0)\
+  //===
+  #include "defprovar.inl"
+  //===
+  //string cdn_file()const{return to_string(game_id);};
+  //string serialize()const{return {};}
+};
+struct t_cdn_game{
+  struct t_player{
+    #define DEF_PRO_COPYABLE()
+    #define DEF_PRO_CLASSNAME()t_player
+    #define DEF_PRO_VARIABLE(ADD)\
+    ADD(string,coder,{})\
+    ADD(string,v,{})\
+    ADD(string,err,{})\
+    ADD(vector<double>,tick2ms,{})\
+    //===
+    #include "defprovar.inl"
+    //===
+  };
+  #define DEF_PRO_COPYABLE()
+  #define DEF_PRO_CLASSNAME()t_cdn_game
+  #define DEF_PRO_VARIABLE(ADD) \
+  ADD(t_game_decl,gd,{})\
+  ADD(t_finished_game,fg,{})\
+  ADD(vector<t_player>,slot2player,{})\
+  ADD(vector<vector<string>>,tick2cmds,{})\
+  //===
+  #include "defprovar.inl"
+  //===
+  string serialize()const{return QapSaveToStr(*this);}
+  int size()const{return serialize().size();}// TODO: need return serialize().size() but optimized!
+};
+struct t_game{
+  #define DEF_PRO_COPYABLE()
+  #define DEF_PRO_CLASSNAME()t_game
+  #define DEF_PRO_VARIABLE(ADD)\
+  ADD(t_game_decl,gd,{})\
+  ADD(t_finished_game,fg,{})\
+  ADD(string,ordered_at,{})\
+  ADD(string,finished_at,{})\
+  ADD(string,status,"new")\
+  ADD(string,author,{})\
+  //===
+  #include "defprovar.inl"
+  //===
+};
 
 struct QueuedGame {
   t_game_decl decl;
@@ -477,71 +543,6 @@ struct t_world{
   //void step(){};
   //bool finished(){return false;}
   vector<double> slot2score;
-};
-struct t_game_uploaded_ack{
-  #define DEF_PRO_COPYABLE()
-  #define DEF_PRO_CLASSNAME()t_game_uploaded_ack
-  #define DEF_PRO_VARIABLE(ADD)\
-  ADD(int,game_id,{})\
-  ADD(string,err,{})\
-  //===
-  #include "defprovar.inl"
-  //===
-};
-struct t_finished_game{
-  #define DEF_PRO_COPYABLE()
-  #define DEF_PRO_CLASSNAME()t_finished_game
-  #define DEF_PRO_VARIABLE(ADD)\
-  ADD(int,game_id,{})\
-  ADD(vector<double>,slot2ms,{})\
-  ADD(vector<double>,slot2score,{})\
-  ADD(vector<string>,slot2status,{})\
-  ADD(int,tick,0)\
-  ADD(int,size,0)\
-  //===
-  #include "defprovar.inl"
-  //===
-  string cdn_file()const{return to_string(game_id);};
-  string serialize()const{return {};}
-};
-struct t_cdn_game{
-  struct t_player{
-    #define DEF_PRO_COPYABLE()
-    #define DEF_PRO_CLASSNAME()t_player
-    #define DEF_PRO_VARIABLE(ADD)\
-    ADD(string,coder,{})\
-    ADD(string,v,{})\
-    ADD(string,err,{})\
-    ADD(vector<double>,tick2ms,{})\
-    //===
-    #include "defprovar.inl"
-    //===
-  };
-  #define DEF_PRO_COPYABLE()
-  #define DEF_PRO_CLASSNAME()t_cdn_game
-  #define DEF_PRO_VARIABLE(ADD) \
-  ADD(t_finished_game,fg,{})\
-  ADD(vector<t_player>,slot2player,{})\
-  ADD(vector<vector<string>>,tick2cmds,{})\
-  //===
-  #include "defprovar.inl"
-  //===
-  string serialize()const{return QapSaveToStr(*this);}
-  int size()const{return serialize().size();}// TODO: need return serialize().size() but optimized!
-};
-struct t_game{
-  #define DEF_PRO_COPYABLE()
-  #define DEF_PRO_CLASSNAME()t_game
-  #define DEF_PRO_VARIABLE(ADD)\
-  ADD(t_game_decl,gd,{})\
-  ADD(t_finished_game,fg,{})\
-  ADD(string,ordered_at,{})\
-  ADD(string,finished_at,{})\
-  ADD(string,status,"new")\
-  ADD(string,author,{})\
-  //===
-  #include "defprovar.inl"
-  //===
 };
 struct t_main : t_process,t_http_base {
   vector<t_coder_rec> carr;mutex carr_mtx;vector<size_t> ai2cid;
