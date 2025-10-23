0e5a30a90ca37ade5eb6ceb2feb25bd636f3ac35
adler3d <adler3d@gmail.com>
2025-10-23 09:29:05 +0300

    upd

--- aicup/QapLR/QapLR/QapLR.cpp
+++ aicup/QapLR/QapLR/QapLR.cpp
@@ -2574,11 +2574,13 @@ public:
   void BR(){y-=ident;x=bx;}
   void AddText(const string&text)
   {
+    EM_ASM({console.log("&text",$0)},&text);
+    EM_ASM({console.log("text.size()",$0)},text.size());
     EM_ASM({console.log("AddText::this",$0)},this);
     EM_ASM({console.log("AddText::LV.size()",$0)},LV.size());
     int n=sizeof(LV);
     for(int i=0;i<n;i+=4){
-      EM_ASM({console.log("AddText::LV::int",$0,$1)},((int*)&LV)[i]);
+      EM_ASM({console.log("AddText::LV::int",$0)},((int*)&LV)[i]);
     }
     LV.push_back(TextLine(x,y,text));
     BR();
